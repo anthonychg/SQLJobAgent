6660e95d42678414e03630ba112bbe23bf96c7d3
AC <37120385+anthonychg@users.noreply.github.com>
2019-09-18 22:17:12 -0700

    Add files via upload

---
+++ SQLAgentStartJob.sql
@@ -0,0 +1,21 @@
+--SQL JOB AGENT MANAGEMENT
+--Start Job When Not Running:
+DECLARE @JOB_NAME SYSNAME = N'SQLJobName'; 
+ 
+IF NOT EXISTS(     
+        select 1 
+        from msdb.dbo.sysjobs_view job  
+        inner join msdb.dbo.sysjobactivity activity on job.job_id = activity.job_id 
+        where  
+            activity.run_Requested_date is not null  
+        and activity.stop_execution_date is null  
+        and job.name = @JOB_NAME 
+        ) 
+BEGIN      
+    PRINT 'Starting job ''' + @JOB_NAME + ''''; 
+    EXEC msdb.dbo.sp_start_job @JOB_NAME; 
+END 
+ELSE 
+BEGIN 
+    PRINT 'Job ''' + @JOB_NAME + ''' is already started '; 
+END
